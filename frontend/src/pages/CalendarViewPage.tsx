52a28c4515df6ed2de276fb54245c7966f65acf1
Jimbo Liao <fong.milk@gmail.com>
2024-03-19 16:39:11 +0800

    add calendar view

--- frontend/src/pages/CalendarViewPage.tsx
+++ frontend/src/pages/CalendarViewPage.tsx
@@ -1,5 +1,38 @@
+import { Box, styled } from "@mui/material";
+import WorkspaceTitle from "../components/WorkspaceTitle";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+const StyledContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  width: "100%",
+  paddingTop: "20px",
+  paddingBottom: "20px",
+  paddingLeft: "50px",
+  paddingRight: "50px",
+});
+
 const CalendarViewPage = () => {
-  return <div>calendarview</div>;
+  return (
+    <StyledContainer>
+      <WorkspaceTitle title="Calendar" />
+      <Box sx={{ width: "100%", height: "100%" }}>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          height="100%"
+          initialView="dayGridMonth"
+        />
+      </Box>
+    </StyledContainer>
+  );
 };
 
 export default CalendarViewPage;
