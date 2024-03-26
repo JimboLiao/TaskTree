a0ff334e43cff55ed745aa06baf2546cf26c1399
Jimbo Liao <fong.milk@gmail.com>
2024-03-26 11:01:55 +0800

    modify Tree for TaskTableModal

--- frontend/src/components/Tree.tsx
+++ frontend/src/components/Tree.tsx
@@ -1,17 +1,31 @@
 import { Box } from "@mui/material";
 import TreeNode from "./TreeNode";
+import TaskTableModal from "./TaskTableModal";
+import { useState } from "react";
 
 //@todo interface
 const Tree = ({ treeData }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
       <Box>
-        {treeData.map((node) => (
-          <TreeNode node={node} />
+        {treeData.map((node, index) => (
+          <TreeNode key={index} node={node} onEdit={handleEditTreeNode} />
         ))}
       </Box>
+      <TaskTableModal isModalOpen={isModalOpen} onModalClose={handleClose} />
     </>
   );
+
+  function handleEditTreeNode() {
+    setIsModalOpen(true);
+  }
+  function handleClose() {
+    setIsModalOpen(false);
+    // @todo close modal
+    // send request to api to change update task data
+  }
 };
 
 export default Tree;
