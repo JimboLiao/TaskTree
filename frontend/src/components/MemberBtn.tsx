6bb2b1ae7ec9c4e2d51484b4e8500cb9d0a59134
Jimbo Liao <fong.milk@gmail.com>
2024-04-13 18:53:43 +0800

    refactor for modal display

---
+++ frontend/src/components/MemberBtn.tsx
@@ -0,0 +1,58 @@
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../contexts/UserContext";
+
+const MemberBtn: React.FC = () => {
+  const [anchor, setAnchor] = useState<HTMLElement | null>(null);
+  const { logout } = useUserContext();
+  const navigate = useNavigate();
+  const open = Boolean(anchor);
+  return (
+    <>
+      <Button
+        color="inherit"
+        id="member-button"
+        aria-controls={open ? "member-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Member
+      </Button>
+      <Menu
+        id="member-menu"
+        anchorEl={anchor}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={handleProfile}>Profile</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    setAnchor(event.currentTarget);
+  }
+  function handleClose() {
+    setAnchor(null);
+  }
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
+  function handleProfile() {
+    navigate("/profile");
+  }
+};
+
+export default MemberBtn;
