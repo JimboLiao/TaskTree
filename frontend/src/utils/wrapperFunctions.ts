4932bd283ec11cdbf06ee15e7c44e46ad633f35c
Jimbo Liao <fong.milk@gmail.com>
2024-05-25 14:53:53 +0800

    update wrapper functions

--- frontend/src/utils/wrapperFunctions.ts
+++ frontend/src/utils/wrapperFunctions.ts
@@ -6,13 +6,18 @@ import {
   ImportanceColor,
   TaskDetailResponse,
   TaskDetail,
+  Task,
+  Attendee,
 } from "../api/taskAPI";
+import { Category } from "../api/categoryAPI";
+import { User } from "../api/userAPI";
 
 function wrapTaskInfo(d: TaskInfoResponse): TaskInfo {
   const { category, resources, ...task } = d;
 
   const categoryColor = category[0].color;
   const categoryId = category[0].id;
+  const categoryName = category[0].name;
 
   return {
     ...task,
@@ -26,17 +31,60 @@ function wrapTaskInfo(d: TaskInfoResponse): TaskInfo {
     updateTime: dayjs(task.updateTime),
     categoryId,
     categoryColor,
+    categoryName,
     statusColor: StatusColor[d.status],
     importanceColor: ImportanceColor[d.importance],
     resources,
   };
 }
 
+function wrapAttendee(attendee: { id: number; user: User }): Attendee {
+  return { attendeeId: attendee.id, ...attendee.user };
+}
+
 function wrapTaskDetail(d: TaskDetailResponse): TaskDetail {
   const { category, attendee, ...task } = d;
   const taskInfo = wrapTaskInfo({ category, ...task });
-  const categoryName = category[0].name;
-  return { ...taskInfo, attendees: attendee.map((a) => a.user), categoryName };
+  return {
+    ...taskInfo,
+    attendees: attendee.map((a) => wrapAttendee(a)),
+  };
 }
 
-export { wrapTaskInfo, wrapTaskDetail };
+function unwrapTaskDetail(d: TaskDetail) {
+  const { attendees, resources, ...t } = d;
+  const category: Category = {
+    id: t.categoryId,
+    name: t.categoryName,
+    color: t.categoryColor,
+  };
+  const att = attendees.map((a) => ({
+    id: a.attendeeId,
+    user: {
+      id: a.id,
+      email: a.email,
+      username: a.username,
+      createTime: a.createTime,
+      updateTime: a.updateTime,
+    },
+  }));
+  const task: Task = {
+    id: t.id,
+    title: t.title,
+    description: t.description,
+    start: t.start.toISOString(),
+    end: t.end.toISOString(),
+    isAllDay: t.isAllDay,
+    status: t.status,
+    importance: t.importance,
+    reminder: t.reminder,
+    location: t.location,
+    createTime: t.createTime.toISOString(),
+    updateTime: t.updateTime.toISOString(),
+    parentTaskId: t.parentTaskId,
+  };
+
+  return { attendees: att, resources, category, task };
+}
+
+export { wrapTaskInfo, wrapAttendee, wrapTaskDetail, unwrapTaskDetail };
