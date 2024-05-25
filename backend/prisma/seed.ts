67788a57b1fe3dac9dafff7aea9900cb162e2a3a
Jimbo Liao <fong.milk@gmail.com>
2024-05-25 14:26:54 +0800

    update prisma seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -13,18 +13,39 @@ async function main() {
   console.log("Prisma seeding...");
 
   // delete all data
+  await prisma.taskOfUser.deleteMany();
+  await prisma.resource.deleteMany();
+  await prisma.category.deleteMany();
   await prisma.user.deleteMany();
   await prisma.token.deleteMany();
+  await prisma.task.deleteMany();
 
   const hashpassword = await bcrypt.hash(dummyUser.password, 10);
   const date = new Date();
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email: dummyUser.email,
       password: hashpassword,
       createTime: date,
       updateTime: date,
     },
+  });
+
+  await prisma.category.createMany({
+    data: [
+      {
+        name: "Work",
+        gCalendarId: null,
+        color: "#1983FF",
+        userId: user.id,
+      },
+      {
+        name: "Travel",
+        gCalendarId: null,
+        color: undefined,
+        userId: user.id,
+      },
+    ],
   });
 }
 
