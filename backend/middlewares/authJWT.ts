55dba0ac0108806d0cbb2132888000875bad5302
Jimbo Liao <fong.milk@gmail.com>
2024-05-31 16:32:36 +0800

    modify authJWT middleware

--- backend/middlewares/authJWT.ts
+++ backend/middlewares/authJWT.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, {
+  JwtPayload,
+  TokenExpiredError,
+  JsonWebTokenError,
+  NotBeforeError,
+} from "jsonwebtoken";
 import { UnauthorizedError } from "../utils/errors/customErrors";
 
 interface tokenPayload extends JwtPayload {
@@ -19,7 +24,15 @@ const authJWT = async (req: Request, res: Response, next: NextFunction) => {
     res.locals.id = decoded.id;
     next();
   } catch (err) {
-    next(err);
+    if (err instanceof TokenExpiredError) {
+      next(new UnauthorizedError("JWT has expired"));
+    } else if (err instanceof JsonWebTokenError) {
+      next(new UnauthorizedError("Invalid JWT"));
+    } else if (err instanceof NotBeforeError) {
+      next(new UnauthorizedError("JWT not active"));
+    } else {
+      next(err);
+    }
   }
 };
 
