42c9a8579b9c6da161f4e986669896cda63ce5d6
Jimbo Liao <fong.milk@gmail.com>
2024-06-13 09:41:08 +0800

    socket connection

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
 import router from "./routes";
 import errorHandler from "./middlewares/errorHandler";
 import cors from "cors";
@@ -24,6 +26,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/1.0/", router);
 initSwagger(app);
 app.use(errorHandler);
-app.listen(port, () => {
+
+app.get("/test", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+// Create an HTTP server and integrate Socket.IO
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
